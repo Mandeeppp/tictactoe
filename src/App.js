74390fd91bd1ec3a9c44c663b2815d5b2941aff1
MandeepKaur <gsmandeep1111@gmail.com>
2022-09-11 23:34:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// import logo from './logo.svg';
+import './App.css'
+export default function App(){
+  return(
+    <div className="App">
+      
+
+    </div>
+  );  
+
+}
+function Board() {
+  return (
+    <div className="Board">
+      <Block></Block>
+
+    </div>
+  )
+}
+function Block(){
+  return (
+    <div className="Block dot">
+
+    </div>
+  )
+}
